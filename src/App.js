4c0e2a1fb58f27ea542ad07cfc60947e2e40bad4
Your Name <you@example.com>
2020-11-24 13:15:12 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          !!! יא גבר
+          אין צדיק כבן עמרם!
         </p>
         <a
           className="App-link"
